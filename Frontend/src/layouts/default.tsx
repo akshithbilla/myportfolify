b170a33e246d1e21f5839022c4aaef9f186b33c6
Billa Akshith <billaakshith@gmail.com>
2025-06-19 21:52:13 +0530

    Updated config

--- Frontend/src/layouts/default.tsx
+++ Frontend/src/layouts/default.tsx
@@ -17,7 +17,7 @@ export default function DefaultLayout({
         <Link
           isExternal
           className="flex items-center gap-1 text-current"
-          href="https://akshithbilla-portfolio.vercel.app/projects"
+          href="https://akshithbilla.vercel.app/"
           title="heroui.com homepage"
         >
           <span className="text-default-600">© 2025 </span>
